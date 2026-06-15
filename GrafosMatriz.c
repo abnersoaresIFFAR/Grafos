8e84f23ed5f5140b5eae82f38c46f843170148da
abnersoaresIFFAR <abner.92092@aluno.iffar.edu.br>
2026-06-15 15:14:06 -0300

    Matrizes: Definida as variáveis globais do arquivo e criada a função de inicialização do grafo.

--- GrafosMatriz.c
+++ GrafosMatriz.c
@@ -3,11 +3,24 @@
 #include <string.h>
 #include "GrafosMatriz.h"
 
-//variável global local
-
+static int EH_PONDERADO;
+static int EH_DIRIGIDO;
+static int QUANTIDADE_VERTICES;
+static GrafoM* raiz[100];
+static float matrizAdj[100][100];
 
 void inicializarGrafoMatriz(int eh_ponderado, int eh_dirigido){
-    printf("Função Não Implementada\n");
+    // Inicia a matriz de adjacência
+    for(int i = 0; i < 100; i++){
+        for(int j = 0; j < 100; j++){
+            matrizAdj[i][j] = 0.0;
+        }
+        raiz[i] = NULL;  // Inicia o array de vértices
+    }
+
+    EH_PONDERADO = eh_ponderado;
+    EH_DIRIGIDO = eh_dirigido;
+    QUANTIDADE_VERTICES = 0;
 }
 
 void adicionarVerticeMatriz(int id, char* texto){
